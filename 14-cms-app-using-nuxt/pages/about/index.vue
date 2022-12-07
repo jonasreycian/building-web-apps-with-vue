dd6eec517d6fef7293f4eb7db80bf2bdde866752
Jonas Reycian B. Saraosos <jonas@madmonkeyhostels.com>
2022-12-07 23:58:46 +0800

    Adding pages

---
+++ 14-cms-app-using-nuxt/pages/about/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container-fluid">
+        <h2>About</h2>
+        <p>Hello</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt soluta inventore quia dignissimos non
+            itaque? Accusantium iure minus itaque sit cum magnam voluptas, totam recusandae corrupti asperiores ratione,
+            voluptatibus deserunt!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
