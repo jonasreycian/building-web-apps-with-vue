868a5381f21ab51c6b159f312a5ab4d4ad86ff44
Jonas Reycian B. Saraosos <jonas@madmonkeyhostels.com>
2022-12-13 20:49:29 +0800

    Handled error pages

--- 14-cms-app-using-nuxt/pages/products/[id].vue
+++ 14-cms-app-using-nuxt/pages/products/[id].vue
@@ -11,6 +11,16 @@ const uri = "https://fakestoreapi.com/products/" + id;
 // Fetch the product
 const { data: product } = await useFetch(uri, { key: id });
 
+console.log(product);
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product not found!",
+    fatal: true,
+  });
+}
+
 definePageMeta({
   layout: "products",
 });
