c5749b73f559c31edc34f82fc1d7e1b32c07bb01
Jonas Reycian B. Saraosos <jonas@madmonkeyhostels.com>
2022-12-13 19:13:13 +0800

    Added tailwind css support

---
+++ 14-cms-app-using-nuxt/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <nav class="container mx-auto p-4 flex justify-between">
+        <nuxt-link to="/" class="font-bold">Nasu Dojo</nuxt-link>
+        <ul class="flex gap-4">
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/products">Products</nuxt-link></li>
+          <li><nuxt-link to="/about" class="btn">About</nuxt-link></li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- Output the page content -->
+    <div class="container p-4 mx-auto">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
---
+++ 14-cms-app-using-nuxt/pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Products details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora, labore
+      veniam quas neque ipsam voluptate quae! In quod temporibus sapiente minus.
+      Dolore corporis voluptatibus fugiat quisquam nostrum nobis inventore quis.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
